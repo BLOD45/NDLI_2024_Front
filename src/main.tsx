07a9fa6ae5b8fb3443aca3803a19ff95e6f14007
Cyril ONFRAY <onfray.e2204577@etud.univ-ubs.fr>
2024-12-05 20:16:24 +0100

    fix: modification du systeme de directories

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import CauchemardCookieBasicCount from './CookieCauchemard/Cookie.tsx'
+import CauchemardCookieBasicCount from './pages/CookieCauchemard/Cookie.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
