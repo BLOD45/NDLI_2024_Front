23352e019c15fb98a01c0bd0322a41bdd116d42d
Cyril ONFRAY <onfray.e2204577@etud.univ-ubs.fr>
2024-12-05 22:21:44 +0100

    feat: Début de clicker de mort

--- src/pages/CookieCauchemard/Cookie.tsx
+++ src/pages/CookieCauchemard/Cookie.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import Cookie from '../../components/CookieCauchemard/CookieComponent.tsx'
+import CookieShop from '../../components/CookieCauchemard/CookieShop.tsx'
+import './Cookie.css'
 
 function CauchemardCookieBasicCount() {
+    const [count, setCount] = useState(0)
+    const [boolCookie, setBoolCookie] = useState(false)
+
     return (
-        <>
-            <div>
+        <div className="container">
+            <CookieShop count={count} boolCookie={boolCookie} />
+            <div className="clicker-zone">
                 <h1>Cauchemard Cookie</h1>
-                <Cookie />
+                <Cookie count={count} boolCookie={boolCookie} setCount={setCount} setBoolCookie={setBoolCookie} />
             </div>
-        </>
-    );
+        </div>
+    )
 }
 
 export default CauchemardCookieBasicCount
