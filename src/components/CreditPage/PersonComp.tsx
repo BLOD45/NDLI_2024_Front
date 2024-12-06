cb24eccfe9fb3eb05e34fbbdeb7d20c58d1145e8
Cerzen <103956414+Alan-Le-Gourrierec@users.noreply.github.com>
2024-12-06 06:47:21 +0100

    Merge credit page

---
+++ src/components/CreditPage/PersonComp.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+// import 'nes.css/css/nes.min.css';
+import "./PersonComp.css"
+
+interface PersonCompProps {
+    name: string; // username a afficher
+    image: string; // Chemin de la pp
+    role: string; //Role a afficher
+}
+
+function PersonComp({ name, image, role }: PersonCompProps) {
+    return (
+        <div className="is-dark person-comp nes-container is-rounded">
+            <div className="profile-pic-container nes-container is-rounded">
+                <img src={image}/>
+            </div>
+            <div className="nes-badge note">
+                <span className="is-error">{role}</span>
+            </div>
+
+            <p className="name nes-text is-success"> &lt;{name}&gt; </p>
+        </div>
+    );
+}
+
+export default PersonComp;
