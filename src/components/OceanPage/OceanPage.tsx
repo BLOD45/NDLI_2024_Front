3727ba1792229880f8b415d3da2c1136c1cd6611
Hypolite <lagoutte.e2109481@etud.univ-ubs.fr>
2024-12-06 04:11:41 +0100

    prototype popup

--- src/components/OceanPage/OceanPage.tsx
+++ src/components/OceanPage/OceanPage.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 
-
 const OceanPage: React.FC = () => {
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [popupContent, setPopupContent] = useState('');
 
+    const handleButtonClick = (content: string) => {
+        setPopupContent(content);
+        setPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setPopupVisible(false);
+    };
 
     return (
         <>
@@ -11,19 +20,65 @@ const OceanPage: React.FC = () => {
                 style={{
                     height: "100%",
                     width: "100%",
+                    position: "relative",
                     backgroundImage: "url('/src/assets/images/principal/test.jpg')",
                     backgroundSize: "cover",
                     backgroundPosition: "center"
                 }}
             >
-            </div>
+                {/* Visible Buttons */}
+                <button
+                    style={{
+                        position: "absolute",
+                        top: "20%",
+                        left: "30%",
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: "rgba(255, 0, 0, 0.5)", // Semi-transparent red
+                        border: "none",
+                        cursor: "pointer"
+                    }}
+                    onClick={() => handleButtonClick('Popup Content 1')}
+                >
+                    Button 1
+                </button>
+                <button
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "60%",
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: "rgba(0, 0, 255, 0.5)", // Semi-transparent blue
+                        border: "none",
+                        cursor: "pointer"
+                    }}
+                    onClick={() => handleButtonClick('Popup Content 2')}
+                >
+                    Button 2
+                </button>
 
-            <div>
-
+                {/* Popup */}
+                {popupVisible && (
+                    <div
+                        style={{
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            padding: "20px",
+                            backgroundColor: "white",
+                            border: "1px solid black",
+                            zIndex: 1000
+                        }}
+                    >
+                        <p>{popupContent}</p>
+                        <button onClick={closePopup}>Close</button>
+                    </div>
+                )}
             </div>
         </>
-    )
-}
-
+    );
+};
 
 export default OceanPage;
