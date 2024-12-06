e0c1cc816ca9e3a6afdf34dd0686c2e256ca8c5f
DryloXx2 <nicolas.e2002568@etud.univ-ubs.fr>
2024-12-06 07:15:34 +0100

    Revert "Merge branch 'develop' into production"
    This reverts commit fbe7cca0dd3ddd5e097c94e8f042f0cb8f73b56e, reversing
    changes made to 3cc04ba34fa4ef390769c2a77e71622d5af3a2ff.

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,8 @@ import App from './App.tsx'
 import CauchemardCookieBasicCount from './pages/CookieCauchemard/Cookie.tsx'
 import Principale from './pages/Principale/Principale.tsx';
 import Captcha from './pages/Captcha/Captcha.tsx';
+import Credits from './pages/Credits/Credits.tsx';
 import ChoixPage from './pages/ChoixCreditOuCookie/ChoixCreditOuCookie.tsx';
-import CreditPage from './pages/Credits/CreditsPage.tsx'
-import 'nes.css/css/nes.min.css';
 
 
 createRoot(document.getElementById('root')!).render(
@@ -20,7 +19,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/Principale" element={<Principale />} />
         <Route path="/Captcha" element={<Captcha />} />
         <Route path="/ChoixPage" element={<ChoixPage />} />
-        <Route path="/Credits" element={<CreditPage />} />
+        <Route path="/Credits" element={<Credits />} />
         <Route path="/CauchemardCookie" element={<CauchemardCookieBasicCount />} />
       </Routes>
     </BrowserRouter>
