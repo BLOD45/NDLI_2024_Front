68ca5015c5e27a78428f2ed8efe15800f8b920d3
Fyne <mary.e2204591@etud.univ-ubs.fr>
2024-12-05 22:02:57 -0500

    Update HumanPage

--- src/components/HumanPage/HumanPage.tsx
+++ src/components/HumanPage/HumanPage.tsx
@@ -3,7 +3,6 @@ import HumanImage from "../../assets/images/principal/human.png";
 import InfoPopUp from "../InfoPopUp/InfoPopUp";
 import texts from "../../assets/text/texts.json";
 
-
 const HumanPage: React.FC = () => {
     const [isVisible, setIsVisible] = useState(false);
     const [popupContent, setPopupContent] = useState<React.ReactNode>(null);
@@ -27,25 +26,27 @@ const HumanPage: React.FC = () => {
     };
 
     return (
-        <div style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
-            {/* Colonne avec les boutons */}
-            <div style={{ display: "flex", flexDirection: "column", marginRight: "20px" }}>
+        <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+            {/* Colonne avec les boutons, fixée à une largeur de 150px */}
+            <div style={{ display: "flex", flexDirection: "column", marginRight: "20px", width: "150px", textAlign: "center" }}>
                 <button onClick={() => handleButtonClick("1")}>1</button>
                 <button onClick={() => handleButtonClick("2")}>2</button>
                 <button onClick={() => handleButtonClick("6")}>6</button>
             </div>
 
-            {/* Image principale */}
-            <div>
-                <img src={HumanImage} alt="Human" style={{ width: "100%" }} />
+            {/* Image principale, centrée et fixée à une taille de 400px */}
+            <div style={{ width: "400px", textAlign: "center" }}>
+                <img src={HumanImage} alt="Human" style={{ width: "100%", height: "auto" }} />
             </div>
 
-            {/* Affichage du pop-up */}
-            <InfoPopUp
-                isVisible={isVisible}
-                content={popupContent}
-                onClose={() => setIsVisible(false)}
-            />
+            {/* Colonne du pop-up, avec un max-width pour éviter que le texte déborde */}
+            <div style={{ width: "300px", marginLeft: "20px" }}>
+                <InfoPopUp
+                    isVisible={isVisible}
+                    content={popupContent}
+                    onClose={() => setIsVisible(false)}
+                />
+            </div>
         </div>
     );
 };
