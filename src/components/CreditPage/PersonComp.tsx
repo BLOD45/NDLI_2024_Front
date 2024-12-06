fc2818821e9bd99178e420d9b9a827cb88e91ea3
DryloXx2 <nicolas.e2002568@etud.univ-ubs.fr>
2024-12-06 06:54:52 +0100

    fix dependancies

--- src/components/CreditPage/PersonComp.tsx
+++ src/components/CreditPage/PersonComp.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-// import 'nes.css/css/nes.min.css';
 import "./PersonComp.css"
 
 interface PersonCompProps {
--- src/pages/Credits/CreditsPage.tsx
+++ src/pages/Credits/CreditsPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Credit from "../../components/CreditPage/PersonComp";
 import "./CreditsPage.css"
 
